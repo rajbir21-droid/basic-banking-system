e5e03df9d2776ecff14f30cf7e3f6cf24d7522a7
rajbir21-droid <banerjeerajbir2888@gmail.com>
2021-08-16 02:42:55 +0530

    updatenavbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav">
         <li className="nav-item active">
-          <Link className="nav-link" to="/home">Home</Link>
+          <Link className="nav-link" to="/">Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link" to="/customers">Customers</Link>
